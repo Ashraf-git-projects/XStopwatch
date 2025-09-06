e9be54840770a4753073bd2673970d5ab7f7937c
Ashraf <ashrafhussain2265@gmail.com>
2025-09-06 14:29:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [isRunning , setIsRunning] = useState(false);
+  const [elapsedTime , setElapsedTime] = useState(0);
+
+  useEffect(()=>{
+    let intervalID;
+    if(isRunning){
+       intervalID = setInterval(()=>{
+         setElapsedTime((prevElapsedTime)=>prevElapsedTime + 1);
+       } , 1000);
+    }
+    return ()=> clearInterval(intervalID);
+  },[isRunning]);
+
+
+  const startStop = ()=>{
+     setIsRunning((prevIsRunning) => !prevIsRunning);
+  };
+
+  const reset = ()=>{
+   setIsRunning(false);
+   setElapsedTime(0);
+  }
+  const formatTime = (seconds)=>{
+     const minutes = Math.floor(seconds / 60);
+     const remainingSeconds = seconds % 60;
+     return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  }
+
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+      <h4>Time:{formatTime(elapsedTime)}</h4>
+      <button onClick={startStop} >{isRunning ? "Stop" : "Start"}</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
